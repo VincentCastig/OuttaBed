b6be190c211d82415ba6e1d635594895d6202961
Vincent Castigliola <vincentcastigliola@randallreilly.com>
2020-11-19 18:49:36 -0600

    create user sql supdate

--- db/createUser.sql
+++ db/createUser.sql
@@ -1,2 +1,2 @@
-INSERT INTO device_table(device_id, token) VALUES($1,$2) ON CONFLICT (device_id) DO UPDATE SET token = EXCLUDED.token
+INSERT INTO device_table(device_id, token) VALUES($1,$2)
 RETURNING *
