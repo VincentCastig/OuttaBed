85bfa25020ec09bcf8412b7c59db583699a77276
Vincent Castigliola <vincentcastigliola@randallreilly.com>
2021-01-15 11:52:26 -0600

    include data to push notification

--- src/api/expoServer.js
+++ src/api/expoServer.js
@@ -77,7 +77,7 @@ module.exports = {
                         sound: 'default',
                         body: quotes[randomNumber].quote,
                         title: quotes[randomNumber].author,
-                        data: {withSome: 'data'},
+                        data: quotes[randomNumber],
                     })
                 }
             }
