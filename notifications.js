5a861f7178ed04274132d3dbf7bec80a80e33f31
VincentCastig <vincent.castig@gmail.com>
2025-06-25 18:20:04 -0500

    update project

--- notifications.js
+++ notifications.js
@@ -1,37 +1,41 @@
-import { AsyncStorage } from 'react-native';
-import * as Permissions from 'expo-permissions';
+import { Platform } from 'react-native';
 import * as Notifications from 'expo-notifications';
-import axios from 'axios';
 import Constants from 'expo-constants';
 
 const registerForPushNotificationsAsync = async () => {
-    if (Constants.isDevice) {
-        const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-        let finalStatus = existingStatus;
-        if (existingStatus !== 'granted') {
-            console.log('existingStatus');
-            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-            finalStatus = status;
-        }
-        if (finalStatus !== 'granted') {
-            //alert('In order to receive notifications, go to your settings page and enable push totifications');
-            return 'false';
-        }
-        const token = await Notifications.getExpoPushTokenAsync();
+  if (!Constants.isDevice) {
+    alert('Must use physical device for Push Notifications');
+    return 'false';
+  }
 
-        return token;
-    } else {
-        alert('Must use physical device for Push Notifications');
-    }
+  // Request notification permissions
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
 
-    if (Platform.OS === 'android') {
-        Notifications.setNotificationChannelAsync('default', {
-            name: 'default',
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C',
-        });
-    }
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+
+  if (finalStatus !== 'granted') {
+    // Permissions not granted
+    return 'false';
+  }
+
+  // Get the Expo push token
+  const token = await Notifications.getExpoPushTokenAsync();
+
+  // Android specific channel setup
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 };
 
 export default registerForPushNotificationsAsync;
